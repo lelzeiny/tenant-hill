3ce955a0c5837809f92b75260feacbd6c85a06ca
lelzeiny <leena.elzeiny@gmail.com>
2021-01-02 22:31:33 -0800

    merge ;-;

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,12 @@ import plus_icon from './add.png';
 import './App.css';
 import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, Form, Button, FormControl, InputGroup, Modal } from 'react-bootstrap';
+import { Navbar, Nav, Form, Button, FormControl, InputGroup, Modal, Card, OverlayTrigger, Popover } from 'react-bootstrap';
 import StarRatingComponent from 'react-star-rating-component';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, useLocation } from 'react-router-dom';
+import Upload from './pages/Upload.js';
+import Home from './pages/Home.js';
+import axios from 'axios';
 
 class NavBar extends React.Component{
   render(){
@@ -14,7 +17,7 @@ class NavBar extends React.Component{
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Navbar.Brand href="/2424-Haste">Tenant Hill</Navbar.Brand>
+            <Navbar.Brand href="/apartment?id=2424-Haste">Tenant Hill</Navbar.Brand>
             <Nav.Link href="/">Check a Price</Nav.Link>
             <Nav.Link href="/">Find Deals</Nav.Link>
           </Nav>
@@ -54,17 +57,55 @@ class HomePage extends React.Component {
   }
 }
 
+class LabelText extends React.Component{
+  render(){
+    return(
+      <div style={{display:"flex"}}>
+        <p style={{fontWeight: "bold", color: "#6181b0"}}>{this.props.before}</p>
+        <p>&nbsp;{this.props.after}</p>
+      </div>
+    );
+  }
+}
+
+class ZipcodePage extends React.Component {
+  render() {
+    return (
+      <div id="zipcode-page" className="page-container">
+        <div className="welcome" id="bg-2">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1>Find Apartments Near You</h1>
+          <Form inline>
+            <FormControl type="text" placeholder="Zipcode" className="mr-sm-2" />
+            <Button variant="warning">Search</Button>
+          </Form>
+        </div>
+      </div>
+    );
+  }
+}
+
 class Review extends React.Component{
   render(){
     return(
       <div className = "review">
         <div style={{display:'flex', alignItems: 'center'}}>
-          <p style={{fontWeight: "bold"}}>{this.props.name}&nbsp;</p>
+            <OverlayTrigger trigger="click" placement="right" overlay=
+            {<Popover id="popover-basic">
+              <Popover.Title as="h3">Contact Information</Popover.Title>
+              <Popover.Content>
+              k8+SQZqPRj4!v$-f@gmail.com 
+              </Popover.Content>
+            </Popover>}>
+              <p style={{fontWeight: "bold", cursor:"pointer"}}>{this.props.name}&nbsp;</p>
+            </OverlayTrigger>
+          
           
             <StarRatingComponent className="star"
+              name="display"
               editing={false}
               starCount={5}
-              value={this.props.rating}
+              value={parseInt(this.props.rating)}
             />
         </div>
           <p>{this.props.description}</p>
@@ -112,8 +153,8 @@ function CreateListing() {
               placeholder="Square footage"
             />
           </InputGroup>
-          <label class="container">&nbsp;&nbsp;Laundry<input type="checkbox"/> <span class="checkmark"></span></label>
-          <label class="container">&nbsp;&nbsp;Wifi<input type="checkbox"/> <span class="checkmark"></span></label>
+          <label className="container">&nbsp;&nbsp;&nbsp;Laundry<input type="checkbox"/> <span className="checkmark"></span></label>
+          <label className="container">&nbsp;&nbsp;&nbsp;Wifi<input type="checkbox"/> <span className="checkmark"></span></label>
           <br/>
         </Modal.Body>
         <Modal.Footer>
@@ -175,51 +216,170 @@ function CreateReview() {
   );
 }
 
-class AddressPage extends React.Component {
-  render() {
-    return (
-      <div id="address-page" className="page-container">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <div className="two-tone-text">
-          <span className="blueText">The Deal</span>&nbsp;<span className="blackText">with {this.props.address}</span>
+function AddressPage(props) {
+  const { search } = useLocation();
+  const match = search.match(/id=(.*)/);
+  const id = match?.[1];
+
+  //send query to backend
+  let apt_data = {
+    title: "Jones Berkeley",
+    price: 2100,
+    bed: 3,
+    dog: true,
+    cat: true,
+    address: "1500 San Pablo Ave Berkeley, CA 94702",
+    gym:false,
+    dishwasher: true,
+    ac: true,
+    laundry: true,
+    parking: false,
+    estimatedPrice: 2000,
+    img_url: "",
+    ratings:{
+      "1230841234":{
+        rating: 1,
+        fullname: "John Doe",
+        description:"Makes for a good potato",
+        contact: "$n$6N^GDs5P&XMR-@gmail.com"
+      },
+      "6234532454":{
+        rating: 5,
+        fullname: "Nhat Nguyen",
+        description:"Landlord didn’t give back the deposit, but also was always able to accomodate my wife and I throughout the pandemic. The water heater also leaks.",
+        contact: "k8+SQZqPRj4!v$-f@gmail.com"
+      }
+    }
+  };
+  
+  //send query to backend
+  const our_rating = "Good"
+  const avg_rating = 68;
+
+  return (
+    <div id="address-page" className="page-container">
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <div className="two-tone-text">
+        <span className="blueText">The Deal</span>&nbsp;<span className="blackText">with {apt_data["address"]}</span>
+      </div>
+      <div id="main-ratings">
+        <div id="picture">
+          <img src={apt_data["imgUrl"]} height="350"/>
         </div>
-        <div id="main-ratings">
-          <div id="picture">
-            <img src={this.props.picture} height="350"/>
-          </div>
+        <div className="main-cols">
+          <h4 className="blackTextSmall"> List Price</h4>
+          <div className="pricing"><h1>{apt_data["price"]}</h1></div>
+          
+          
           <div className="rating-container">
-            <h4>Our Rating</h4>
-            <div className="rating"><h1>{this.props.our_rating}</h1></div>
+            <h4 className="blackTextSmall">The Anthill rated this...</h4>
+            
+            <div className="rating"><h1>{our_rating}</h1></div>
             <a href="#our-rating">See why &rsaquo;</a>
           </div>
+        </div>
+        <div className="main-cols">
+          
+          <h4 className="blackTextSmall" > Estimated Price</h4>
+          <div className="pricing"><h1>{apt_data["estimatedPrice"]}</h1></div>
+
           <div className="rating-container">
-            <h4>The People's Rating</h4>
-            <div className="rating"><h1>{this.props.people_rating}</h1></div>
+            <h4 className="blackTextSmall">People rated this...</h4>
+            <div className="rating"><h1>{avg_rating}%</h1></div>
             <a href="#people-rating">See why &rsaquo;</a>
           </div>
         </div>
-        <div id="our-rating">
-          <div className="two-tone-text">
-            <span className="blueText">Our</span>&nbsp;&nbsp;<span className="blackText">Rating</span>
-          </div>
-          <p>Your listing is valued at {this.props.actual_price} whereas we estimated the cost to be 
-          {this.props.estimated_price}. This is because it has {this.props.details}</p>
-        </div>
-        <div id="people-rating">
-          <div className="inline">
-            <div className="two-tone-text">
-              <span className="blueText">The People's</span>&nbsp;&nbsp;<span className="blackText">Rating</span>
-            </div>
-            <CreateReview />
-          </div>
-          <Review rating="1" name="John Doe" description="Makes for a good potato"></Review>
-          <Review rating="5" name="Nhat Nguyen" description="Landlord didn’t give back the deposit, but also was always able to accomodate my wife and I throughout the pandemic. The water heater also leaks."></Review>
-        </div>
       </div>
+      <div id="our-rating">
+        <div className="two-tone-text">
+          <span className="blueText">Our</span>&nbsp;&nbsp;<span className="blackText">Rating</span>
+        </div>
+        <p className="darkBlueTextSmall">Your listing is valued at {apt_data["actual_price"]} whereas we estimated 
+        the cost to be {apt_data["estimatedPrice"]}. We based our evaluation on the following properties:</p>
+        <LabelText before="Dogs Allowed:" after={apt_data["dog"].toString()}/>
+        <LabelText before="Cats Allowed:" after={apt_data["cat"].toString()}/>
+        <LabelText before="Indoor Gym:" after={apt_data["gym"].toString()}/>
+        <LabelText before="Dishwasher:" after={apt_data["dishwasher"].toString()}/>
+        <LabelText before="AC:" after={apt_data["ac"].toString()}/>
+        <LabelText before="Laundry:" after={apt_data["laundry"].toString()}/>
+      </div>
+      <div id="people-rating">
+        <div className="inline">
+          <div className="two-tone-text">
+            <span className="blueText">The People's</span>&nbsp;&nbsp;<span className="blackText">Rating</span>
+          </div>
+          <CreateReview />
+        </div>
+        {Object.values(apt_data["ratings"]).map(rating => (
+          <Review rating={rating["rating"]} name={rating["fullname"]} description={rating["description"]}></Review>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+class HouseCard extends React.Component{
+  render(){
+    return(
+      <Card style={{ width: '18rem', margin: "10px"}}>
+        <Card.Img variant="top" src={this.props.picture} />
+        <Card.Body>
+          <Card.Title class="card-title">{this.props.address}</Card.Title>
+          <Card.Text>
+            <LabelText key="Actual Price:" value={"$" + this.props.actual_price}/>
+            <LabelText key="Estimated Value:" value={"$" + this.props.est_price}/>
+          </Card.Text>
+          <Button variant="warning">More</Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
 
+function HouseListingsPage(){
+  const { search } = useLocation();
+  const match = search.match(/zipcode=(.*)/);
+  const zipcode = match?.[1];
+  let listings = {
+    "1234123545":{
+      price: 1200,
+      estimatedPrice: 1400,
+      address: "1500 San Pablo Ave Berkeley, CA 94702",
+      imgUrl: "https://images1.apartments.com/i2/7kCLBEyMGq-247cAWJ3Iklr29Z06oUTRATGlL4sbZHc/111/berkeley-central-berkeley-ca-primary-photo.jpg"
+    },
+    "1234143545":{
+      price: 1200,
+      estimatedPrice: 1400,
+      address: "1500 San Pablo Ave Berkeley, CA 94702",
+      imgUrl: "https://images1.apartments.com/i2/7kCLBEyMGq-247cAWJ3Iklr29Z06oUTRATGlL4sbZHc/111/berkeley-central-berkeley-ca-primary-photo.jpg"
+    },
+    "1236143545":{
+      price: 1200,
+      estimatedPrice: 1400,
+      address: "1500 San Pablo Ave Berkeley, CA 94702",
+      imgUrl: "https://images1.apartments.com/i2/7kCLBEyMGq-247cAWJ3Iklr29Z06oUTRATGlL4sbZHc/111/berkeley-central-berkeley-ca-primary-photo.jpg"
+    },
+    "1234129545":{
+      price: 1200,
+      estimatedPrice: 1400,
+      address: "1500 San Pablo Ave Berkeley, CA 94702",
+      imgUrl: "https://images1.apartments.com/i2/7kCLBEyMGq-247cAWJ3Iklr29Z06oUTRATGlL4sbZHc/111/berkeley-central-berkeley-ca-primary-photo.jpg"
+    }
+  }
+  //send query to backend
+  return(
+    <div id="listings-page" className="page-container">
+      <div className="two-tone-text">
+      <span className="blackText">Exuber</span><span className="blueText">Ant</span>&nbsp;<span className="blackText">Deals in {zipcode}</span>
+      </div>
+      <div id="listings">
+        {Object.values(listings).map(apt => (
+          <HouseCard  actual_price={apt["price"].toString()} est_price={apt["estimatedPrice"].toString()} address={apt["address"]} picture={apt["imgUrl"]}/>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 function App() {
   document.title = 'Tenant Hill';
@@ -228,8 +388,14 @@ function App() {
       <NavBar />
       <BrowserRouter>
         <Switch>
-          <Route path="/2424-Haste">
-            <AddressPage picture="https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F1026205392%2F0x0.jpg" address="2424 Haste Street" our_rating="Good" people_rating="68%"/>
+          <Route path="/apartment">
+          <AddressPage picture="https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F1026205392%2F0x0.jpg"/>
+          </Route>
+          <Route path="/zipcode">
+            <ZipcodePage />
+          </Route>
+          <Route path="/listings">
+            <HouseListingsPage zipcode="95070"/>
           </Route>
           <Route path="/">
             <HomePage />
